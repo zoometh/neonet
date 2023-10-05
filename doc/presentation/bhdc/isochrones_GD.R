6e3e018424701d1ea61e5333dadca0d7291c6e83
zoometh <thomashuet7@gmail.com>
2023-10-05 20:36:08 +0100

    isochrons

--- doc/presentation/bhdc/isochrones_GD.R
+++ doc/presentation/bhdc/isochrones_GD.R
@@ -6,30 +6,9 @@ source("R/neo_isochr.R")
 source("R/neo_spd.R")
 
 drive_find(n_max = 30)
-
-drive_ls(path = "BHDC")
 td <- tempdir()
 myc14data <- paste0(td, "/output.geojson")
 drive_download("2_AOI_France_SE-W.geojson", path = myc14data, overwrite = T)
 
-# myc14data <- "https://raw.githubusercontent.com/zoometh/neonet/main/results/1_AOI_France_E-W.geojson"
 neo_isochr(df.c14 = myc14data, mapname = "France_SE-W")
 neo_isochr(df.c14 = myc14data, mapname = "France_SE-W", selected.per = c("LM"))
-
-# example: C:\Users\Thomas Huet\AppData\Local\Temp\RtmpOq0fVQ
-
-
-source("R/neo_subset.R")
-source("R/neo_bib.R")
-source("R/neo_matlife.R")
-source("R/neo_calib.R")
-source("R/neo_merge.R")
-source("R/neo_html.R")
-source("R/neo_datamiss.R")
-source("R/neo_datasum.R")
-source("R/neo_doi.R")
-
-
-
-neo_isochr(df.c14 = "https://raw.githubusercontent.com/zoometh/neonet/main/results/neonet-data-2023-09-24.geojson", 
-           show.lbl = FALSE)
