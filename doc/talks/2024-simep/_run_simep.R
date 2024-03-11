ae6a387c8b8eac3719bde7919e4633e8341fb9ab
zoometh <thomashuet7@gmail.com>
2024-03-11 17:58:54 +0000

    date to remove

--- doc/talks/2024-simep/_run_simep.R
+++ doc/talks/2024-simep/_run_simep.R
@@ -193,4 +193,4 @@ ggplot2::ggsave(map.iso$map,
                 height = 14)
 
 source("R/neo_find_dates.R")
-neo_find_dates(df = map.iso$data, idf.dates = c(591))
+neo_find_dates(df = map.iso$data, idf.dates = c(118))
