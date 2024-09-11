9f9b2eef46b903e650e3eb7e29a6dc00ab467006
zoometh <thomashuet7@gmail.com>
2024-09-11 21:05:04 +0100

    varia

--- doc/run_outliers.R
+++ doc/run_outliers.R
@@ -32,6 +32,10 @@ where <- sf::st_read(where.roi,
 col.c14baz <- c("sourcedb", "site", "labnr", "c14age", "c14std", "period", "culture", "lon", "lat")
 samp_df <- read.csv("https://raw.githubusercontent.com/zoometh/neonet/main/doc/talks/2024-simep/df14_simep_4.csv")
 df.c14 <- samp_df
+# correct coordinates (# Sabha)
+source("R/neo_dbs_coord_dates.R")
+df.c14 <- neo_dbs_coord_dates(df.c14, verbose = FALSE)
+
 df.c14 <- sf::st_as_sf(df.c14, coords = c("lon", "lat"), crs = 4326)
 
 source("R/neo_dbs_rm_date.R")
@@ -58,7 +62,7 @@ isochr$map
 
 source("R/neo_find_date.R")
 source("R/neo_dbs_info_date.R")
-abber.date <- neo_find_date(df = isochr$data, idf.dates = 766)
+abber.date <- neo_find_date(df = isochr$data, idf.dates = 93)
 ad <- neo_dbs_info_date(df.c14 = df.c14, LabCode = abber.date$labcode)
 # Do not add double quotes in the https://github.com/zoometh/neonet/blob/main/inst/extdata/c14_aberrant_dates.tsv file
 
