32057ffcf336f06f2015b0297568c0b9bb354558
zoometh <thomashuet7@gmail.com>
2024-09-11 20:10:27 +0100

    varia

--- R/neo_dbs_coord_dates.R
+++ R/neo_dbs_coord_dates.R
@@ -3,6 +3,7 @@
 #' @description Update the coordinates of sites.
 #'
 #' @param df A dataframe of dates
+#' @param SiteName The column of the site name in df. Default: SiteName.
 #' @param c14.corrected.coords Path to a TSV having corrected coordinates. 
 #' @param verbose if TRUE (default) then display different messages.
 #'
@@ -14,8 +15,16 @@
 #'
 #' @export
 neo_dbs_coord_dates <- function(df = NA,
-                                c14.corrected.coords = "https://raw.githubusercontent.com/zoometh/neonet/main/inst/extdata/c14_corrected_coordinates.tsv"){
+                                SiteName = "SiteName",
+                                c14.corrected.coords = "https://raw.githubusercontent.com/zoometh/neonet/main/inst/extdata/c14_corrected_coordinates.tsv",
+                                verbose = TRUE){
   df.cor.coords <- read.csv2(c14.corrected.coords, sep = "\t", header = TRUE)
-  subset(df_filtered, SiteName == 'Sabha')
+  for(i in seq(1, nrow(df.cor.coords))){
+    site <- df.cor.coords[i, "site"]
+    if(verbose){
+      print(paste0("Read new coordinates for the site: ", site))
+    }
+  }
+  subset(samp_df, SiteName == 'Sabha')
 }
 
