ae21a668045a18f765c4f68f8805224bc9941a8e
zoometh <thomashuet7@gmail.com>
2024-10-03 18:18:52 +0100

    tidying files and foldres

--- doc/clim/temp.R
+++
@@ -1,7 +0,0 @@
-# Create a basic plot
-plot(x = 1:10, y = (1:10)^2, type = "b", 
-     main = "Example Plot with Vertical Line", 
-     xlab = "X-axis", ylab = "Y-axis")
-
-# Add a vertical line at x = 5
-abline(v = 5, col = "red", lwd = 2, lty = 2)
