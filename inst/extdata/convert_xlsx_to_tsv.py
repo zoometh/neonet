0c6791ce6fdd94c294e7b5c2069f2475fc77df48
zoometh <thomashuet7@gmail.com>
2024-10-09 21:41:15 +0100

    create a workflow to export ref_table_per.xlsx to TSV

---
+++ inst/extdata/convert_xlsx_to_tsv.py
@@ -0,0 +1,21 @@
+import openpyxl
+import csv
+import os
+
+print(os.getcwd())
+
+xlsx_filename = 'inst/extdata/ref_table_per.xlsx'
+tsv_filename = 'inst/extdata/ref_table_per-readonly.tsv'
+
+# xlsx_filename = 'erms-template.xlsx'
+# tsv_filename = 'erms-template-readonly.tsv'
+
+wb = openpyxl.load_workbook(xlsx_filename)
+sheet = wb.active
+
+with open(tsv_filename, 'w', newline='') as tsv_file:
+    tsv_writer = csv.writer(tsv_file, delimiter='\t')
+    for row in sheet.iter_rows(values_only=True):
+        tsv_writer.writerow(row)
+
+print(f'Conversion from XLSX to TSV complete. TSV file saved as {tsv_filename}')
